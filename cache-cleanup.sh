3915ef4df32267ba1ec3e2ca8052ebfcb0b2daad
Klaus Purer <klaus.purer@protonmail.ch>
2022-11-20 11:30:50 +0100

    fix(cache): Print curl response for debugging

--- cache-cleanup.sh
+++ cache-cleanup.sh
@@ -3,11 +3,12 @@
 # Deletes a bunch of cache entries in Github Actions. We only need the latest
 # cache entry, which will be written after the Action run.
 
-CACHE_IDS=( $(curl \
+RESPONSE=$(curl -i \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN"  \
-  https://api.github.com/repos/klausi/mts-cron/actions/caches \
-  | grep id | grep -o '[0-9]*') )
+  https://api.github.com/repos/klausi/mts-cron/actions/caches )
+echo "$RESPONSE"
+CACHE_IDS=( $(echo "$RESPONSE" | grep '"id"' | grep -o '[0-9]*') )
 
 for CACHE_ID in "${CACHE_IDS[@]}"
 do
